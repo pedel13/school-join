339de4998d1445ce526552ecabffd8834ce4a11c
iamfrdfa <102164209+iamfrdfa@users.noreply.github.com>
2024-05-07 12:47:27 +0200

    added logo transition (not finished yet)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 function init() {
 
 }
+
+//Function show and hide belong together and are for the Join Logo
+function show() {
+    document.getElementById('big-logo').style.display = 'flex';
+    document.getElementById('header').style.display = 'none';
+    document.getElementById('loginWrapper').style.display = 'none';
+    setTimeout("hide()", 2000);
+}
+function hide() {
+    document.getElementById('big-logo').style.display = 'none';
+    document.getElementById('header').style.display = 'flex';
+    document.getElementById('loginWrapper').style.display = 'flex';
+}
