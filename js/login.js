901a0987e1e6ecd27e8e98376696016645f7c325
iamfrdfa <102164209+iamfrdfa@users.noreply.github.com>
2024-05-30 11:21:49 +0200

    preparation for user-registration & login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,13 @@
 let users = [
-    {'email': 'admin', 'password': '123456'}
+    {'email': 'admin@join.de', 'password': '123456'},
+    {'email': 'test@test.de', 'password': 'testUser'}
 ];
 function loginValidation() {
 
+}
+
+function guestLogin(event) {
+    event.preventDefault();
+    document.getElementById('loginEmail').value = "test@test.de";
+    document.getElementById('loginPassword').value = "testUser";
 }
--- js/register.js
+++ js/register.js
@@ -1,5 +1,23 @@
 function addUser() {
+    let name = document.getElementById("name");
     let email = document.getElementById("email");
     let password = document.getElementById("password");
-    users.push({email: email.value, password: password.value});
+    let passwordConfirm = document.getElementById("passwordConfirm");
+    let checkBox = document.getElementById("registerCheckbox");
+    
+    if (password.value === passwordConfirm.value) {
+        if (checkBox.checked) {
+            users.push({email: email.value, password: password.value});
+        }
+        else if (checkBox === !checkBox.checked) {
+            window.alert("Bitte den haken Privacy akzeptieren!");
+        }
+        window.alert("Du hast dich erfolgreich registriert!");
+    }
+    else window.alert("Passwörter stimmen nicht überein!");
+    
+    name.value = "";
+    email.value = "";
+    password.value = "";
+    passwordConfirm.value = "";
 }
