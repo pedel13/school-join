99ebd2c44f29cc18d6619d9c5fa71a0935b1b52d
iamfrdfa <102164209+iamfrdfa@users.noreply.github.com>
2024-10-16 21:55:40 +0200

    small corrections

--- js/template.js
+++ js/template.js
@@ -306,10 +306,12 @@ function renderTaskEditor(taskId, task) {
                                 <div class="addTaskEnterSubtask d-flex align-items-center justify-content-between">
                                     <input type="text" id="subtasks" placeholder="Enter Subtasks"/>
                                     <button class="d-flex" id="addTaskAddSubtaskButton" type="button" onclick="addSubtaskAddArray()">
-                                    <div class="addTaskAdd"></div>
+                                        <div class="addTaskAdd"></div>
                                     </button>
                                 </div>
-                                <div id="massageBoxSubtask" class="d-none"><span class="alert-massage">Please type something</span>
+                                <div id="massageBoxSubtask" class="d-none">
+                                    <span class="alert-massage">Bitte gib eine Aufgabe ein</span>
+                                </div>
                                 <ul id="subtaskStorage"></ul>
                             </div>
                         </div>
